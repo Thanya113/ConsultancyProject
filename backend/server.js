5536b0c65e32b82974694efdf5359a14c99cc88c
Thanya113 <118598797+Thanya113@users.noreply.github.com>
2024-04-13 10:52:25 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import cors from 'cors';
 
 const port = process.env.PORT || 5000;
 
@@ -17,6 +18,7 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
